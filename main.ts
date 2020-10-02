0b3a2c16b53517b814339c764e5fa374f8efe843
Damian-Gluszynski <71399957+Damian-Gluszynski@users.noreply.github.com>
2020-10-02 13:11:03 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let Distance_To_Object = 0
+basic.showIcon(IconNames.Yes)
+basic.pause(500)
 basic.forever(function () {
-	
+    Distance_To_Object = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(Distance_To_Object)
 })
